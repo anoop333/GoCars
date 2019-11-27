425a5ee15eca8d60459fd1e47d853ce3a1666394
Muhammedyaseen09 <0906@yaseen>
2019-11-27 16:20:28 +0530

    kl

---
+++ app/src/main/java/gocars/mainproject/stc/service4.java
@@ -0,0 +1,14 @@
+package gocars.mainproject.stc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class service4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service4);
+    }
+}
